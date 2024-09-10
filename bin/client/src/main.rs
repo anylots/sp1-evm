bb891c2ba477b69e677f3b44a9ad7852dc6427b7
anylots <username@users.noreply.github.com>
2024-09-10 18:07:17 +0800

    trace convert

--- bin/client/src/main.rs
+++ bin/client/src/main.rs
@@ -6,8 +6,11 @@ use morph_executor::verify;
 use sbv_primitives::types::BlockTrace;
 
 pub fn main() {
+    // Read the input.
     let x = sp1_zkvm::io::read::<String>();
     let trace: BlockTrace = serde_json::from_str(&x).unwrap();
+
+    // Execute the block.
     let pi_hash = verify(&trace).unwrap();
 
     // Commit to the public values of the program. The final proof will have a commitment to all the
--- crates/morph-executor/src/lib.rs
+++ crates/morph-executor/src/lib.rs
@@ -3,80 +3,20 @@ use std::rc::Rc;
 use alloy::primitives::keccak256;
 use sbv_core::{EvmExecutorBuilder, HardforkConfig, VerificationError};
 use sbv_primitives::{types::BlockTrace, zk_trie::ZkMemoryDb, Block, B256};
-use sbv_utils::{
-    cycle_track, dev_error, dev_info, dev_trace, measure_duration_histogram, update_metrics_counter,
-};
+use sbv_utils::{cycle_track, dev_error, dev_info};
 
 pub fn verify(l2_trace: &BlockTrace) -> Result<B256, VerificationError> {
-    dev_trace!("{l2_trace:#?}");
-
-    // Step1: verify blob
+    // Step1. verify blob
     // TODO
     let versioned_hash: Vec<u8> = vec![];
 
-    // Step2: Execute all transactions and get stateroot
+    // Step2. Execute all transactions
     let root_before = l2_trace.root_before();
     let root_after = l2_trace.root_after();
+    let withdraw_root = l2_trace.withdraw_root();
+    let sequencer_set_root: Vec<u8> = vec![];
 
-    let fork_config: HardforkConfig = HardforkConfig::default_from_chain_id(2818);
-
-    // or with v2 trace
-    // let v2_trace = BlockTraceV2::from(l2_trace.clone());
-
-    // or with rkyv zero copy
-    // let serialized = rkyv::to_bytes::<BlockTraceV2, 4096>(&v2_trace).unwrap();
-    // let archived = unsafe { rkyv::archived_root::<BlockTraceV2>(&serialized[..]) };
-    // let archived = rkyv::check_archived_root::<BlockTraceV2>(&serialized[..]).unwrap();
-
-    #[cfg(feature = "profiling")]
-    let guard = pprof::ProfilerGuardBuilder::default()
-        .frequency(1000)
-        .blocklist(&["libc", "libgcc", "pthread", "vdso"])
-        .build()
-        .unwrap();
-
-    let zktrie_db = cycle_track!(
-        {
-            let mut zktrie_db = ZkMemoryDb::new();
-            measure_duration_histogram!(
-                build_zktrie_db_duration_microseconds,
-                l2_trace.build_zktrie_db(&mut zktrie_db)
-            );
-            Rc::new(zktrie_db)
-        },
-        "build ZktrieState"
-    );
-
-    let mut executor = EvmExecutorBuilder::new(zktrie_db.clone())
-        .hardfork_config(fork_config)
-        .build(&l2_trace)?;
-
-    // TODO: change to Result::inspect_err when sp1 toolchain >= 1.76
-    #[allow(clippy::map_identity)]
-    #[allow(clippy::manual_inspect)]
-    executor.handle_block(&l2_trace).map_err(|e| {
-        dev_error!(
-            "Error occurs when executing block #{}({:?}): {e:?}",
-            l2_trace.number(),
-            l2_trace.block_hash()
-        );
-
-        update_metrics_counter!(verification_error);
-        e
-    })?;
-    let revm_root_after = executor.commit_changes(&zktrie_db);
-
-    #[cfg(feature = "profiling")]
-    if let Ok(report) = guard.report().build() {
-        let dir = std::env::temp_dir()
-            .join(env!("CARGO_PKG_NAME"))
-            .join("profiling");
-        std::fs::create_dir_all(&dir).unwrap();
-        let path = dir.join(format!("block-{}.svg", l2_trace.number()));
-        let file = std::fs::File::create(&path).unwrap();
-        report.flamegraph(file).unwrap();
-        dev_info!("Profiling report saved to: {:?}", path);
-    }
+    let revm_root_after = execute(l2_trace)?;
 
     if root_after != revm_root_after {
         dev_error!(
@@ -85,8 +25,6 @@ pub fn verify(l2_trace: &BlockTrace) -> Result<B256, VerificationError> {
             l2_trace.number(),
             l2_trace.block_hash(),
         );
-
-        update_metrics_counter!(verification_error);
 
         return Err(VerificationError::RootMismatch {
             root_trace: root_after,
@@ -98,9 +36,13 @@ pub fn verify(l2_trace: &BlockTrace) -> Result<B256, VerificationError> {
         l2_trace.number(),
         l2_trace.block_hash(),
     );
+
+    // Step3. Compute pi hash
     let pi_hash = keccak256(
         [
             versioned_hash,
+            withdraw_root.to_vec(),
+            sequencer_set_root,
             root_before.to_vec(),
             revm_root_after.to_vec(),
         ]
@@ -108,4 +50,31 @@ pub fn verify(l2_trace: &BlockTrace) -> Result<B256, VerificationError> {
     );
 
     Ok(B256::from_slice(pi_hash.as_slice()))
+}
+
+fn execute(l2_trace: &BlockTrace) -> Result<alloy::primitives::FixedBytes<32>, VerificationError> {
+    let fork_config: HardforkConfig = HardforkConfig::default_from_chain_id(2818);
+    let zktrie_db = cycle_track!(
+        {
+            let mut zktrie_db = ZkMemoryDb::new();
+            l2_trace.build_zktrie_db(&mut zktrie_db);
+            Rc::new(zktrie_db)
+        },
+        "build ZktrieState"
+    );
+    let mut executor = EvmExecutorBuilder::new(zktrie_db.clone())
+        .hardfork_config(fork_config)
+        .build(&l2_trace)?;
+    #[allow(clippy::map_identity)]
+    #[allow(clippy::manual_inspect)]
+    executor.handle_block(&l2_trace).map_err(|e| {
+        dev_error!(
+            "Error occurs when executing block #{}({:?}): {e:?}",
+            l2_trace.number(),
+            l2_trace.block_hash()
+        );
+        e
+    })?;
+    let revm_root_after = executor.commit_changes(&zktrie_db);
+    Ok(revm_root_after)
 }
